d691eaf53c4d67c29295b77c4e255c9a0d55803a
sachiko <sachiko.idec24@gmail.com>
2023-07-17 23:20:02 +0900

    js追加

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -7,16 +7,28 @@ window.onload = function () {
 // headingにclassを追加する
 window.addEventListener('scroll', function () {
 
-  var targets = document.querySelectorAll(".js-textAnimate");
+  var targets = document.querySelectorAll(".js-h_Animate");
 
   for (var i = 0; i < targets.length; i++) {
     target = targets[i].getBoundingClientRect();
 
-    var eleTop = Math.floor(target.top);
+    var elTop = Math.floor(target.top);
     var h = Math.floor(window.innerHeight * 0.78);
 
-    if (h > eleTop) {
+    if (h > elTop) {
       targets[i].classList.add('is-animate');
     }
   }
+});
+
+// headingを一文字ずつ分割してspanで包む
+var el = document.querySelectorAll('.js-h_Animate');
+
+el.forEach(function (e) {
+  var str = e.textContent;
+  var concatStr = '';
+  for (let i = 0; i < str.length; i++) {
+    concatStr = concatStr + '<span class="char">' + str[i] + '</span>'
+  }
+  e.textContent = concatStr;
 });
